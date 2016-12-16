ee02e4f255d7fe5305fc45bd6ba702fadf6695f0
mohamed salah hassan akel <sci.m.s.hassan@gmail.com>
2016-12-16 17:47:22 +0200

    first activity

---
+++ AndroidStudioProjects/Fastfood/app/src/main/java/CheckNetworkConn.java
@@ -0,0 +1,14 @@
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+//created by mohamed salah
+public class CheckNetworkConn {
+
+
+
+    public boolean isConnected() {
+
+
+    }
+}
